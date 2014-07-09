a1c1891ac60cae4e8259bd8d09b31efaad14880c
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-07-09 17:11:04 -0400

    Updated to turn off testing and enable parallel build

--- paraview/build.sh
+++ paraview/build.sh
@@ -9,10 +9,10 @@ git submodule update --init
 
 # build.
 cmake \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTING=OFF \
     .. | tee cmake.log 2>&1
 
-make | tee make.log 2>&1
+make -j7 | tee make.log 2>&1
 make install | tee install.log 2>&1
 
 # vim: set ai et nu:
