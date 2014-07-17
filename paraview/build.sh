0a06b63664db5580df89038df09b0aacdd367a0b
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-07-17 16:53:04 -0400

    Updated build script for paraview

--- paraview/build.sh
+++ paraview/build.sh
@@ -9,6 +9,22 @@ git submodule update --init
 
 # build.
 cmake \
+    -D "CMAKE_CXX_FLAGS:STRING=-fvisibility=hidden -fvisibility-inlines-hidden ${CFLAGS}" \
+    -D "CMAKE_C_FLAGS:STRING=-fvisibility=hidden ${CXXFLAGS}" \
+    -D "CMAKE_INSTALL_PREFIX=:PATH=$PREFIX" \
+    -D CMAKE_OSX_DEPLOYMENT_TARGET:STRING=${MACOSX_DEPLOYMENT_TARGET} \
+    -D BUILD_TESTING:BOOL=OFF \
+    -D CMAKE_BUILD_TYPE:STRING=RELEASE \
+    -D BUILD_SHARED_LIBS:BOOL=OFF \
+    -D PARAVIEW_ENABLE_PYTHON:BOOL=ON \
+    -D "PYTHON_EXECUTABLE:FILEPATH=${PYTHON}" \
+    -D "PYTHON_INCLUDE_PATH:PATH=$PREFIX/include/python${PY_VER}" \
+    -D "PYTHON_LIBRARY:FILEPATH=$PREFIX/lib/libpython${PY_VER}.${SO_EXT}" \
+    "${SRC_DIR}/SuperBuild" \
+    ..
+
+
+cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTING=OFF \
     .. | tee cmake.log 2>&1
 
