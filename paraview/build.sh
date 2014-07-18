116d101f02080653a062106a522daf09309ecf32
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-07-18 08:51:00 -0400

    Fixed script to build with python enabled

--- paraview/build.sh
+++ paraview/build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # see http://conda.pydata.org/docs/build.html for hacking instructions.
+if [ `uname` == Darwin ]; then
+    SO_EXT='dylib'
+else
+    SO_EXT='so'
+fi
 
 # unpack.
 mkdir build
@@ -17,17 +22,11 @@ cmake \
     -D CMAKE_BUILD_TYPE:STRING=RELEASE \
     -D BUILD_SHARED_LIBS:BOOL=OFF \
     -D PARAVIEW_ENABLE_PYTHON:BOOL=ON \
-    -D "PYTHON_EXECUTABLE:FILEPATH=${PYTHON}" \
-    -D "PYTHON_INCLUDE_PATH:PATH=$PREFIX/include/python${PY_VER}" \
-    -D "PYTHON_LIBRARY:FILEPATH=$PREFIX/lib/libpython${PY_VER}.${SO_EXT}" \
+    -D PYTHON_EXECUTABLE:FILEPATH=$PREFIX/bin/python${PY_VER} \
+    -D PYTHON_INCLUDE_PATH:PATH=$PREFIX/include/python${PY_VER} \
+    -D PYTHON_LIBRARY:FILEPATH=$PREFIX/lib/libpython${PY_VER}.${SO_EXT} \
     "${SRC_DIR}/SuperBuild" \
     ..
-
-
-cmake \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTING=OFF \
-    .. | tee cmake.log 2>&1
-
 make -j7 | tee make.log 2>&1
 make install | tee install.log 2>&1
 
